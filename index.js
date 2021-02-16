a57fbbc6047ba9e176703d712d3ca9db04e41f4e
Jesus Milan <jmilan.gonzalez@gmail.com>
2021-02-16 13:26:43 -0500

    added some questions regarding md file content

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
+
+// prompt lines for inquirer
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your proyect?'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'How can someone install your application?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What is the standard use of this application?'
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Who else worked on this project?'
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'How can someone test this application?'
+    },
+]);
