c2e6066e9cb11ff424111d0d94b6c958f9f38b7d
Jesus Milan <jmilan.gonzalez@gmail.com>
2021-02-16 15:10:06 -0500

    created fuction to put info together in readme file

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# README-File-Generator
+# README Maker
+    
--- index.js
+++ index.js
@@ -1,53 +1,77 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const util = require('util');
+
+const writeFileAsync = util.promisify(fs.writeFile);
 
 // prompt lines for inquirer
-inquirer.prompt([
-    {
-        type: 'input',
-        name: 'title',
-        message: 'What is the title of your proyect?'
-    },
-    {
-        type: 'input',
-        name: 'installation',
-        message: 'How can someone install your application?'
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'What is the standard use of this application?'
-    },
-    {
-        type: 'input',
-        name: 'contribution',
-        message: 'Who else worked on this project?'
-    },
-    {
-        type: 'input',
-        name: 'test',
-        message: 'How can someone test this application?'
-    },
-    {
-        type: 'list',
-        name: 'license',
-        message: 'What license do you want for this application?',
-        choices: [
-            'Apache License 2.0',
-            'MIT License',
-            'Boost Software License 1.0',
-            'Eclipse Public License',
-            'None'
-        ]
-    },
-    {
-        type: 'input',
-        name: 'contactgit',
-        message: 'What is your GitHub username?'
-    },
-    {
-        type: 'input',
-        name: 'contactemail',
-        message: 'What is your email?'
-    },
+function promptUser() {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your proyect?'
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'How can someone install your application?'
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'What is the standard use of this application?'
+        },
+        {
+            type: 'input',
+            name: 'contribution',
+            message: 'Who else worked on this project?'
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: 'How can someone test this application?'
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'What license do you want for this application?',
+            choices: [
+                'Apache License 2.0',
+                'MIT License',
+                'Boost Software License 1.0',
+                'Eclipse Public License',
+                'None'
+            ]
+        },
+        {
+            type: 'input',
+            name: 'contactgit',
+            message: 'What is your GitHub username?'
+        },
+        {
+            type: 'input',
+            name: 'contactemail',
+            message: 'What is your email?'
+        },
 ]);
+}
+
+// README file template
+function mdFileTemplate(data) {
+    return `
+    #${data.title}
+    `
+}
+
+// function to put generated file together
+async function init() {
+    try {
+        const data = await promptUser();
+        const readMeFile = mdFileTemplate(data);
+        await writeFileAsync("README.md", readMeFile);
+    } catch (err) {
+        console.log(err)
+    }
+}
+init()
